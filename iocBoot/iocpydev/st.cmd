bb95999a35494e88a8e557ac6ce38c6df2f3aa12
1csp3203 <1csp3203@mail.u-tokai.ac.jp>
2024-12-12 14:57:56 +0900

    TOPをICRampcontrollerに変更＆不要な部分の削除

--- iocBoot/iocpydev/st.cmd
+++ iocBoot/iocpydev/st.cmd
@@ -4,7 +4,7 @@
 
 # PYTHONPATH points to folders where Python modules are.
 epicsEnvSet("PYTHONPATH","$(TOP)/python")
-epicsEnvSet("PYTHONPATH","/home/pla/modules/pydevice/python")##Motor.py
+##epicsEnvSet("PYTHONPATH","/home/pla/modules/pydevice/python")##Motor.py
 
 cd ${TOP}
 
@@ -17,20 +17,20 @@ pydev("from Motor2 import PhidgetServoController")
 ##pydev("motor_controller = PhidgetServoController()")
 
 ## ---------ここから
-dbLoadRecords("${TOP}/testApp/Db/pydevtest.db","P=GID:,R=,L=0,A=0")
+dbLoadRecords("${TOP}/testApp/Db/pydevtest.db")
 epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}")
-drvAsynSerialPortConfigure("L0","/dev/ttyUSB0",0,1,0)
-asynSetOption("L0", -1, "baud","9600")
-asynSetOption("L0", -1, "bits","8")
-asynSetOption("L0", -1, "parity", "none") 
-asynSetOption("L0", -1, "stop","1")
-##asynSetOption("L0", -1, "handshake", "xonxoff")
-asynSetOption("L0", -1, "clocal","Y")
+drvAsynSerialPortConfigure("L1","/dev/ttyUSB0",0,1,0)
+asynSetOption("L1", -1, "baud","9600")
+asynSetOption("L1", -1, "bits","8")
+asynSetOption("L1", -1, "parity", "none") 
+asynSetOption("L1", -1, "stop","1")
+##asynSetOption("L1", -1, "handshake", "xonxoff")
+asynSetOption("L1", -1, "clocal","Y")
 ## ------------- ここまで追加
 
 ## Load record instances
 dbLoadRecords("${TOP}/testApp/Db/pydevtest.db")
-dbLoadRecords("${TOP}/testApp/Db/pycalcrectest.db")
+
 
 cd ${TOP}/iocBoot/${IOC}
 
